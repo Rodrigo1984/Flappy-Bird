1715f015ba77ed310580f8e1290c90526c88325e
Rodrigo Alves <32502474+Rodrigo1984@users.noreply.github.com>
2024-07-23 11:23:40 -0300

    configurando abertura das barreiras

---
+++ js/flappy.js
@@ -0,0 +1,30 @@
+function novoElemento(tagName, className) {
+  const elem = document.createElement(tagName);
+  elem.className = className;
+  return elem;
+}
+
+function Barreira(reversa = false) {
+  this.elemento = novoElemento('div', 'barreira');
+
+  const borda = novoElemento('div', 'borda');
+  const corpo = novoElemento('div', 'corpo');
+  this.elemento.appendChild(reversa ? corpo : borda);
+  this.elemento.appendChild(reversa ? borda : corpo);
+
+  this.setAltura = (altura) => (corpo.style.height = `${altura}px`);
+
+  function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras');
+
+    const superior = new Barreira(true);
+    this.inferior = new Barreira(false);
+
+    this.elemento.appendChild(this.superior.elemento);
+    this.elemento.appendChild(this.inferior.elemento);
+
+    this.sortearAbertura = () => {
+      const alturaSuperior = Math.random() * (altura - abertura);
+    };
+  }
+}
